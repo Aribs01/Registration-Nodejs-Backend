bea9327dcd0ca8abcffa105a1241dc961337632b
Aribs01 <abiolaaribisala@gmail.com>
2022-02-17 03:08:57 +0100

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 
-const tasks = require('./router/task')
 const users = require('./router/user')
 
 const connectDB = require('./db/connect')
@@ -11,7 +10,6 @@ const notFound = require('./Middleware/notFound')
 app.use(express.static('./public'))
 app.use(express.json())
 
-// app.use('/api/v1/tasks', tasks)
 app.use('/api/v1/users', users);
 
 app.use(notFound)
--- public/browser-app.js
+++ public/browser-app.js
@@ -16,62 +16,10 @@ const termsCheckboxInputDOM = document.querySelector('.termsCheckbox-input')
 
 const url = '/api/v1/users'
 
-// Load tasks from /api/tasks
-// const showTasks = async () => {
-//   loadingDOM.style.visibility = 'visible'
-//   try {
-//     const {
-//       data: { tasks },
-//     } = await axios.get('/api/v1/tasks')
-//     if (tasks.length < 1) {
-//       tasksDOM.innerHTML = '<h5 class="empty-list">No tasks in your list</h5>'
-//       loadingDOM.style.visibility = 'hidden'
-//       return
-//     }
-//     const allTasks = tasks
-//       .map((task) => {
-//         const { completed, _id: taskID, name } = task
-//         return `<div class="single-task ${completed && 'task-completed'}">
-// <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
-// <div class="task-links">
-
-
-
-// const showUsers = async () => {
-//   loadingDOM.style.visibility = 'visible'
-//   try {
-//     const {
-//       data: { users },
-//     } = await axios.get('/api/v1/users')
-//     if (users.length < 1) {
-//       usersDOM.innerHTML = '<h5 class="empty-list">No users in your list</h5>'
-//       loadingDOM.style.visibility = 'hidden'
-//       return
-//     }
-//     const allUsers = users
-//       .map((user) => {
-//         const { firstname, lastname, email } = users
-//         return `<div class="single-user ">
-// <h5><span><i class="far fa-check-circle"></i></span>${firstname}</h5>
-// <div class="task-links">
-
-// </div>
-// </div>`
-//       })
-     
-//   } catch (error) {
-//     usersDOM.innerHTML =
-//       '<h5 class="empty-list">There was an error, please try later....</h5>'
-//   }
-//   loadingDOM.style.visibility = 'hidden'
-// }
-
-// showUsers()
-
 
 form1DOM.addEventListener('submit', async (e) => {
   e.preventDefault()
-  // const {firstname, lastname, email, password, termsCheckbox} = userInputDOM.value
+  
   const firstname = firstnameInputDOM.value
   const lastname = lastnameInputDOM.value
   const email = emailInputDOM.value
